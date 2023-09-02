d2427772dbaa6cb3fe623aefd2c43c105e56a98b
aaron <45775845+Aaronwork1205@users.noreply.github.com>
2023-09-02 16:28:33 -0400

    Update MapView
    - Update MapView with OpenStreet API
    - Small bug fixed.

--- screens/event-detail-screen.js
+++ screens/event-detail-screen.js
@@ -101,7 +101,7 @@
 //   );
 // };
 
-
+import axios from 'axios';
 
 // export default EventDetailScreen;
 
@@ -110,14 +110,43 @@
 
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Linking } from 'react-native';
-import { WebView } from 'react-native-webview';
+
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { HeaderBackButton } from '@react-navigation/elements';
+
+import MapView, { Marker, PROVIDER_OSM } from 'react-native-maps';
+
+
+
+
+
 
 const EventDetailScreen = () => {
   const route = useRoute();
   const navigation = useNavigation();
   const { title, description, location } = route.params;
+  const [coordinates, setCoordinates] = React.useState(null);
+
+  React.useEffect(() => {
+    const encodedLocation = encodeURIComponent(location);
+  
+    axios
+      .get(`https://nominatim.openstreetmap.org/search?format=json&q=${encodedLocation}`)
+      .then((response) => {
+        if (response.data && response.data.length > 0) {
+          const result = response.data[0];
+          const { lat, lon } = result;
+          setCoordinates({ latitude: parseFloat(lat), longitude: parseFloat(lon) });
+        } else {
+          <Text>Location Not Found</Text>
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching coordinates', error);
+      });
+  }, [location]);
+
+
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
@@ -140,25 +169,80 @@ const EventDetailScreen = () => {
     Linking.openURL(googleMapsUrl);
   };
 
+
   return (
     <View style={styles.container}>
       <Text>{title}</Text>
       <Text>{description}</Text>
-      <Text>{location}</Text>
-
-      
+      <Text>Location: {location}</Text>
       <MyLink location={location} text="Open with Google Map" onPress={handleLinkPress} />
-
-      <WebView
-        style={styles.map}
-        source={{
-          uri: `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-            location
-          )}&radius=${5}&near=${encodeURIComponent(location)}`,
-        }}
-      />
+      {coordinates && (
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_OSM} // Set the map provider to OSM
+          initialRegion={{
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude,
+            latitudeDelta: 0.003,
+            longitudeDelta: 0.003,
+          }}
+        >
+          <Marker
+            coordinate={coordinates}
+            title={location}
+            description={location}
+          />
+        </MapView>
+      )}
     </View>
   );
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>{title}</Text>
+  //     <Text>{description}</Text>
+  //     <Text>{location}</Text>
+
+      
+  //     <MyLink location={location} text="Open with Google Map" onPress={handleLinkPress} />
+
+  //     {/* <WebView
+  //       style={styles.map}
+  //       source={{
+  //         uri: `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+  //           location
+  //         )}&radius=${5}&near=${encodeURIComponent(location)}`,
+  //       }}
+  //     /> */}
+
+  //     {/* <MapView 
+  //           style = {styles.map}
+  //           initialRegion={{
+  //             latitude:37.78825,
+  //             longitude: -122.4324,
+  //             latitudeDelta: 0.0922,
+  //             longitudeDelta: 0.0421,
+  //           }}
+  //         /> */}
+  //     {coordinates && (
+  //       <MapView
+  //         style={styles.map}
+  //         provider={PROVIDER_OSM} // Set the map provider to OSM
+  //         initialRegion={{
+  //           latitude: coordinates.latitude,
+  //           longitude: coordinates.longitude,
+  //           latitudeDelta: 0.0922,
+  //           longitudeDelta: 0.0421,
+  //         }}
+  //       >
+  //         <Marker
+  //           coordinate={coordinates}
+  //           title={location}
+  //           description={location}
+  //         />
+  //       </MapView>
+  //     )}
+  //   </View>
+  // );
 };
 
 const styles = StyleSheet.create({
@@ -167,8 +251,8 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   map: {
-    flex: 1,
-    marginTop: 10,
+    width:'100%',
+    height: '100%',
   },
 });
 
